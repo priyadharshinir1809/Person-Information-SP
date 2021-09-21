481bcda8f52908e5b497bd3051d91ee36d19afa2
Priyadharshini Ramesh <>
2021-09-21 10:11:53 +0530

    Java version changed

--- src/test/java/com/impiger/sample/SampleApplicationTests.java
+++ src/test/java/com/impiger/sample/SampleApplicationTests.java
@@ -1,9 +1,15 @@
 package com.impiger.sample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(classes=com.impiger.sample.SampleApplication.class)
+//@RunWith(SpringRunner.class)
+//@DataJpaTest
 class SampleApplicationTests {
 
 	@Test
